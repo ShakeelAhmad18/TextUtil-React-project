465ee2da67f32702eabc56deae14cf0603a55e4f
ShakeelAhmad18 <sa1506758@gmail.com>
2024-02-21 19:33:02 +0500

    make changes

--- proj1/src/App.js
+++ proj1/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import TextForm from './components/TextForm';
+import TextForm from './components/Textform';
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
       <Navbar title="TextUtils"  aboutText="About Us"/>
-      <div className="container my-3">
-        <TextForm heading="Write Text Here to Analyze"/>
+      <div className="container">
+        <TextForm heading="Enter Text Below"/>
       </div>
     </>
   );
--- proj1/src/components/TextForm.js
+++ proj1/src/components/TextForm.js
@@ -1,22 +1,31 @@
-import React,{useState} from 'react'
+import React,{useState} from "react";
 
 export default function TextForm(props) {
-    const handleonclick=()=>{
-        let newtext=text.toUpperCase();
-        settext(newtext)
-   
-       }
-    const handleonchange=(event)=>{
+    const [text,settext]=useState("")
+    const handleClick=()=>{
+        let answer=text.toUpperCase()
+        settext(answer)
+    }
+    const handleChange=(event)=> {
         settext(event.target.value)
     }
-    const [text,settext]=useState('Enter Text Here');
+    const hnadleloclick=()=>{
+        let lower=text.toLowerCase()
+        settext(lower)
+    }
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="htmlFor" value={text} onChange={handleonchange} id="textbox" rows="8"></textarea>
+        <div className="mb-3">
+            <div classname="mb-5">
+                <h2>{props.heading}</h2>
+                <textarea value={text} onChange={handleChange} className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+                <div className="container mt-2">
+                <button className="btn btn-primary" onClick={handleClick}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-1" onClick={hnadleloclick}>Convert Lower Case</button>
+                </div>
             </div>
-            <button className="btn btn-primary" onClick={handleonclick}>Convert into UpperCase</button>
+            <div className="container mt-4">
+                <p>Words: {text.length}</p>
+            </div>
         </div>
     )
 }
